37ce02a81ae07689cfdbbb8b43067f1aedd39121
Leo <leonardo.rodriguez92@gmail.com>
2024-06-04 15:07:21 -0400

    added key prop to delete key

--- components/Keyboard/KeyBoard.js
+++ components/Keyboard/KeyBoard.js
@@ -5,9 +5,14 @@ export default function KeyBoard(props) {
         "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
         "A", "S", "D", "F", "G", "H", "J", "K", "L",
         "ENTER", "Z", "X", "C", "V", "B", "N", "M",
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z" />
-        </svg>
+        {
+            key: "delete",
+            element: (
+                <svg key="delete" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z" />
+                </svg>
+            )
+        }
     ]
 
     const handleClick = (letter) => {
@@ -33,15 +38,27 @@ export default function KeyBoard(props) {
 
     const renderRow = (chars) => {
         return chars.map((char, index) => (
-            <Key
-                key={index}
-                char={char}
-                setGuessedWord={props.setGuessedWord}
-                handleClick={handleClick}
-                submitGuess={props.submitGuess}
-                deleteLetter={deleteLetter}
-                clickable={props.clickable}
-            />
+            typeof char === 'string' ? (
+                <Key
+                    key={index}
+                    char={char}
+                    setGuessedWord={props.setGuessedWord}
+                    handleClick={handleClick}
+                    submitGuess={props.submitGuess}
+                    deleteLetter={deleteLetter}
+                    clickable={props.clickable}
+                />
+            ) : (
+                <Key
+                    key={char.key}
+                    char={char.element}
+                    setGuessedWord={props.setGuessedWord}
+                    handleClick={handleClick}
+                    submitGuess={props.submitGuess}
+                    deleteLetter={deleteLetter}
+                    clickable={props.clickable}
+                />
+            )
         ));
     };
 
